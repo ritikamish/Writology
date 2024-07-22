e21be73c9fb46f9288f2c9c4b6898b55dac84f44
RITIKA MISHRA <i.ritikamish@gmail.com>
2024-07-22 14:15:05 +0530

    writology - blog post

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const jsonServer = require("json-server"); // Importing json-server library
+import jsonServer from "json-server"; // Use ES module syntax
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // You can use any port number here; I chose to use 3001
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
